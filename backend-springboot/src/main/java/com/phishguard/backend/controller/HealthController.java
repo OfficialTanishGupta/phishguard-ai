0e7318785bd1d49516a4394b49828de7a0d7fb20
OfficialTanishGupta <officialtanishgupta@gmail.com>
2026-01-01 21:20:39 +0530

    Add Kafka producer configuration

--- backend-springboot/src/main/java/com/phishguard/backend/controller/HealthController.java
+++ backend-springboot/src/main/java/com/phishguard/backend/controller/HealthController.java
@@ -1,13 +1,23 @@
 package com.phishguard.backend.controller;
 
+import com.phishguard.backend.kafka.KafkaProducerService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
+    private final KafkaProducerService kafkaProducerService;
+
+    public HealthController(KafkaProducerService kafkaProducerService) {
+        this.kafkaProducerService = kafkaProducerService;
+    }
+
     @GetMapping("/health")
     public String healthCheck() {
+
+        kafkaProducerService.sendMessage("Health API was called");
+
         return "PhishGuard AI Backend is running!";
     }
 }
---
+++ backend-springboot/src/main/java/com/phishguard/backend/kafka/KafkaProducerService.java
@@ -0,0 +1,20 @@
+package com.phishguard.backend.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducerService {
+
+    private static final String TOPIC = "user.real.behavior";
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send(TOPIC, message);
+    }
+}
